597b9dcedfd256b8b58e5d6597a1654f3021866e
Kartik Sharma <sharmakartik2322002@gmil.com.noreply.github.com>
2023-08-09 16:00:09 +0530

    Created a funtion named randomNumber
    It is a pseudorandom number generator (PRNG) that
    generates numbers between 0 to 2. The numbers generated
    are intergers.

--- script.js
+++ script.js
@@ -7,8 +7,12 @@
   make a game function that keeps it in a loop until the user desires.
 */
 
+// function to get user choice
 function getUserValue() {
   return prompt("Choose Rock, Paper or Scissors: ", "Rock");
 }
 
-console.log(getUserValue());
+//function to generate a pseudorandom number generator (PRNG) from 0 to 2 (2 included).
+function randomNumber() {
+  return Math.round(Math.random() * 2);
+}
