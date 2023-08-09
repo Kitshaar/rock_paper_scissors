9e0035fdccbfa0409047168c5cd74abd40fb3415
Kartik Sharma <sharmakartik2322002@gmil.com.noreply.github.com>
2023-08-09 17:56:27 +0530

    Created playRound and game functions.
    playRound function plays a round of rock paper
    scissors with the computer.
    
    game function uses for loop to run playRound
    function for 5 times and keeps count of who won
    which round and tells the overall winner.
    
    error: getUserValue function needs a bit of dibugging as
    the game does not work as intented if we get value from the user.

--- script.js
+++ script.js
@@ -9,7 +9,8 @@
 
 // function to get user choice
 function getUserValue() {
-  return prompt("Choose Rock, Paper or Scissors: ", "Rock");
+  let value = prompt("Choose Rock, Paper or Scissors: ", "Rock");
+  return value;
 }
 
 //function to generate a pseudorandom number generator (PRNG) from 0 to 2 (2 included).
@@ -25,6 +26,61 @@ function getComputerChoice(value) {
   } else if (value === 1) {
     return "Paper";
   } else {
-    return "scissors";
+    return "Scissors";
   }
 }
+
+//function playround that plays a round between computer and user.
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    (computerSelection === "Rock" && playerSelection === "paper") ||
+    (computerSelection === "Paper" && playerSelection === "scissors") ||
+    (computerSelection === "Scissors" && playerSelection === "rock")
+  ) {
+    return true;
+  } else if (
+    (computerSelection === "Rock" && playerSelection === "rock") ||
+    (computerSelection === "Paper" && playerSelection === "paper") ||
+    (computerSelection === "Scissors" && playerSelection === "scissors")
+  ) {
+    return "It is a Draw.";
+  } else if (
+    (computerSelection === "Paper" && playerSelection === "rock") ||
+    (computerSelection === "Scissors" && playerSelection === "paper") ||
+    (computerSelection === "Rock" && playerSelection === "scissors")
+  ) {
+    return false;
+  } else {
+    return "Invalid input!";
+  }
+}
+const playerSelection = "rock";
+
+function game() {
+  let playerCounter = 0;
+  let computerCounter = 0;
+  for (index = 0; index < 5; ++index) {
+    const computerSelection = getComputerChoice(randomNumber());
+    let checker = playRound(playerSelection, computerSelection);
+
+    if (checker === true) {
+      console.log(
+        "You win! " + playerSelection + " beats " + computerSelection
+      );
+      ++playerCounter;
+    } else if (checker === false) {
+      console.log(
+        "You lose! " + computerSelection + " beats " + playerSelection
+      );
+      ++computerCounter;
+    } else {
+      console.log(checker);
+    }
+  }
+  playerCounter > computerCounter
+    ? console.log("You won the game!")
+    : console.log("You lost the game");
+}
+
+game();
